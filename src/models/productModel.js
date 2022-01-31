36c9b4cfb61e2e9a487dbe526ef27954cba051ae
WasokSriparat <sriparat123@gmail.com>
2022-01-31 22:01:02 +0700

    first commit

---
+++ src/models/productModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define Product Document Structure
+const productSchema = new Schema({
+    name: String,
+    price: Number,
+    unit_in_stock: Number,
+    // reviews field is an array field
+    reviews: [{
+        star: Number,
+        comment: String
+    }]
+},
+{ 
+    timestamps: true        // for storing time/date of creation
+});
+
+// export Product Schema to be usable in other components
+module.exports = mongoose.model("Product", productSchema);
